c09cc6a61c823f21b62c50f14838de5ca8a2574e
Luis <lvercellih@gmail.com>
2023-09-21 11:51:06 -0500

    Add terraform code and github workflow

---
+++ tf/init.sh
@@ -0,0 +1,7 @@
+rm -rf .terraform*
+
+terraform init \
+  -backend-config="bucket=$TERRAFORM_BACKEND_BUCKET" \
+  -backend-config="key=$TERRAFORM_BACKEND_KEY" \
+  -backend-config="region=$TERRAFORM_BACKEND_REGION" \
+  -input=false
---
+++ tf/vars.tf
@@ -0,0 +1,3 @@
+variable "gibhub_oidc_organization" { default = "HangarLabsNet" }
+variable "gibhub_oidc_repository" { default = "*" }
+variable "github_oidcp_arn" {}
